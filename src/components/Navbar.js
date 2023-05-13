017bb66ce691e6597ee970cf96de17e000a8dd1d
Mueem-Nahid <mueem51@gmail.com>
2023-05-13 19:31:20 +0600

    # Feature - sign in , sign up page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,7 @@
 "use client"
 import {Fragment} from 'react'
 import {Disclosure, Menu, Transition} from '@headlessui/react'
-import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/outline'
-
-const navigation = [
-   {name: 'Home', href: '/', current: true},
-]
+import Link from "next/link";
 
 function classNames(...classes) {
    return classes.filter(Boolean).join(' ')
@@ -13,52 +9,31 @@ function classNames(...classes) {
 
 export default function Navbar() {
    return (
-      <Disclosure as="nav" className="bg-gray-800">
+      <Disclosure as="nav" className="bg-gray-100">
          {({open}) => (
             <>
                <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                  <div className="relative flex h-16 items-center justify-between">
-                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                        {/* Mobile menu button*/}
-                        <Disclosure.Button
-                           className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                           <span className="sr-only">Open main menu</span>
-                           {open ? (
-                              <XMarkIcon className="block h-6 w-6" aria-hidden="true"/>
-                           ) : (
-                              <Bars3Icon className="block h-6 w-6" aria-hidden="true"/>
-                           )}
-                        </Disclosure.Button>
-                     </div>
-                     <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                        <div className="flex flex-shrink-0 items-center ">
-                           <img
-                              className="block h-8 w-8 lg:hidden"
-                              src="/game-insights.png"
-                              alt="Game Insights"
-                           />
-                           <img
-                              className="hidden h-8 w-8 lg:block"
-                              src="/game-insights.png"
-                              alt="Game Insights"
-                           />
-                        </div>
-                        <div className="hidden sm:ml-6 sm:block">
-                           <div className="flex justify-center space-x-4">
-                              {navigation.map((item) => (
-                                 <a
-                                    key={item.name}
-                                    href={item.href}
-                                    className={classNames(
-                                       item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                       'rounded-md px-3 py-2 text-sm font-medium'
-                                    )}
-                                    aria-current={item.current ? 'page' : undefined}
-                                 >
-                                    {item.name}
-                                 </a>
-                              ))}
-                           </div>
+                  <div className="relative flex items-center justify-between">
+                     <div className="flex flex-1 items-center justify-between sm:items-stretch ">
+                        <div className="flex flex-shrink-0 items-center cursor-pointer py-1">
+                           <Link href='/'>
+                              <img
+                                 className="block h-12 w-12 lg:hidden"
+                                 src="/logo.png"
+                                 alt="Game Insights"
+                              />
+                              <img
+                                 className="hidden h-14 w-14 lg:block"
+                                 src="/logo.png"
+                                 alt="Game Insights"
+                              />
+                           </Link>
+                           <a
+                              href='/'
+                              className='text-2xl font-bold p-2'
+                           >
+                              Game Insights
+                           </a>
                         </div>
                      </div>
                      <div
@@ -124,7 +99,7 @@ export default function Navbar() {
                   </div>
                </div>
 
-               <Disclosure.Panel className="sm:hidden">
+               {/*<Disclosure.Panel className="sm:hidden">
                   <div className="space-y-1 px-2 pb-3 pt-2">
                      {navigation.map((item) => (
                         <Disclosure.Button
@@ -141,7 +116,7 @@ export default function Navbar() {
                         </Disclosure.Button>
                      ))}
                   </div>
-               </Disclosure.Panel>
+               </Disclosure.Panel>*/}
             </>
          )}
       </Disclosure>
